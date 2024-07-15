5af22a0d98b22efdf11f0da98f5fc59e74b0e018
박찬규 <cham9906@naver.com>
2024-07-15 13:02:54 +0900

    Backend Initial Commit

---
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/GimisangungApplication.java
@@ -0,0 +1,13 @@
+package com.readwe.gimisangung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimisangungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimisangungApplication.class, args);
+	}
+
+}
---
+++ backend/gimisangung/src/test/java/com/readwe/gimisangung/GimisangungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readwe.gimisangung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimisangungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
