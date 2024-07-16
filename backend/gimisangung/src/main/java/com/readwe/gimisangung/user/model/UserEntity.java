dd426724dcd3b721a200e32052baac977044657d
tpspfkdl <tpspfkdl@gmail.com>
2024-07-16 12:15:42 +0900

    feat: userEntity, signupUserDto

--- backend/gimisangung/src/main/java/com/readwe/gimisangung/user/model/UserEntity.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/user/model/UserEntity.java
@@ -1,4 +1,37 @@
 package com.readwe.gimisangung.user.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(length = 15)
+	private String username;
+
+	@Column(length = 32)
+	private String email;
+
+	@Column(length = 64)
+	private String password;
+
+	@Column(length = 64)
+	private String salt;
 }
--- backend/gimisangung/src/main/java/com/readwe/gimisangung/user/model/dto/SignupUserDto.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/user/model/dto/SignupUserDto.java
@@ -1,4 +1,22 @@
 package com.readwe.gimisangung.user.model.dto;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignupUserDto {
+	@Column(length = 15)
+	private String username;
+
+	@Column(length = 32)
+	private String email;
+
+	@Column(length = 64)
+	private String password;
 }
