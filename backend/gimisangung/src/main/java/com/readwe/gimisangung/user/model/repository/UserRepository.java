df6411deced3a2d7dedc81588865ed8a91d4cd67
tpspfkdl <tpspfkdl@gmail.com>
2024-07-22 15:06:12 +0900

    build: User
    UserRepository의 설정을 변경하고 security를 제거했습니다

--- backend/gimisangung/src/main/java/com/readwe/gimisangung/user/model/repository/UserRepository.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/user/model/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package com.readwe.gimisangung.user.model.repository;
 
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.readwe.gimisangung.user.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
