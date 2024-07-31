995c4386c4f7fc555082891a0893bce51fa1a7cd
박찬규 <cham9906@naver.com>
2024-07-31 14:39:53 +0900

    feat: 디렉토리 수정 기능 예외 구체화

--- backend/gimisangung/src/main/java/com/readwe/gimisangung/directory/model/entity/Directory.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/directory/model/entity/Directory.java
@@ -2,6 +2,7 @@ package com.readwe.gimisangung.directory.model.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PostLoad;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,7 +14,6 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name="directories")
 @ToString(callSuper = true)
 @DiscriminatorValue("D")
