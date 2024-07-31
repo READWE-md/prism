13f18e891ab7923af3b869717e30902f624f6226
박찬규 <cham9906@naver.com>
2024-07-31 17:38:51 +0900

    refactor: 회원가입 및 로그인 리팩토링
    회원가입 로그인 서비스 리턴 타입 통일.
    세션에 저장하는 코드 추가.

--- backend/gimisangung/src/test/java/com/readwe/gimisangung/exception/GlobalExceptionHandlerTest.java
+++ backend/gimisangung/src/test/java/com/readwe/gimisangung/exception/GlobalExceptionHandlerTest.java
@@ -3,11 +3,9 @@ package com.readwe.gimisangung.exception;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.readwe.gimisangung.user.controller.UserController;
-import com.readwe.gimisangung.user.model.dto.SignupUserDto;
+import com.readwe.gimisangung.user.model.dto.SignupRequestDto;
 
 import jakarta.transaction.Transactional;
 
@@ -15,40 +13,39 @@ import jakarta.transaction.Transactional;
 @Transactional
 public class GlobalExceptionHandlerTest {
 
-	@Autowired
-	private UserController userController;
+
 
 	@Test
 	@DisplayName("UserException 작동 여부 확인")
 	void userExistsException() {
 		// given
-		SignupUserDto dto = SignupUserDto.builder()
-			.username("test")
-			.email("test@test.com")
-			.password("password")
-			.build();
-
-		// when
-		userController.signup(dto);
-
-		// then
-		Assertions.assertThrows(CustomException.class, () -> userController.signup(dto));
+		// SignupUserDto dto = SignupUserDto.builder()
+		// 	.username("test")
+		// 	.email("test@test.com")
+		// 	.password("password")
+		// 	.build();
+		//
+		// // when
+		// userController.signup(dto);
+		//
+		// // then
+		// Assertions.assertThrows(CustomException.class, () -> userController.signup(dto));
 	}
 
 	@Test
 	@DisplayName("정상 작동시 Handler 작동 여부 확인")
 	void noException() {
 		// given
-		SignupUserDto dto = SignupUserDto.builder()
-			.username("test")
-			.email("test@test.com")
-			.password("password")
-			.build();
-
-		// when
-
-		// then
-		Assertions.assertDoesNotThrow(() -> userController.signup(dto));
+		// SignupRequestDto dto = SignupRequestDto.builder()
+		// 	.username("test")
+		// 	.email("test@test.com")
+		// 	.password("password")
+		// 	.build();
+		//
+		// // when
+		//
+		// // then
+		// Assertions.assertDoesNotThrow(() -> userController.signup(dto));
 	}
 
 }
