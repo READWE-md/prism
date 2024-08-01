fe678f5191b0ffacf4d358cb06bb58124aff2217
박찬규 <cham9906@naver.com>
2024-08-01 16:46:32 +0900

    refact: RequestBody 클래스 삭제

--- backend/gimisangung/src/main/java/com/readwe/gimisangung/util/FastAPIClient.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/util/FastAPIClient.java
@@ -9,22 +9,28 @@ import org.springframework.web.client.RestTemplate;
 
 public class FastAPIClient {
 
-	private static final String URI = "http://localhost:3000";
+	private static final String URI = "http://localhost:3000/contract/";
 
 	private final RestTemplate restTemplate = new RestTemplate();
 	private final HttpHeaders headers = new HttpHeaders();
 
-	private HttpEntity<RequestBody> httpEntity;
+	private Long contractId;
+	private List<String> images;
 
 	public FastAPIClient() {
 		headers.setContentType(MediaType.APPLICATION_JSON);
 	}
 
-	private void addRequestBody(RequestBody requestBody) {
-		httpEntity = new HttpEntity<>(requestBody, headers);
+	private void setContractId(Long contractId) {
+		this.contractId = contractId;
+	}
+
+	private void setImages(List<String> images) {
+		this.images = images;
 	}
 
 	private String sendRequest() {
-		return restTemplate.postForObject(URI, httpEntity, String.class);
+		HttpEntity<List<String>> httpEntity = new HttpEntity<>(images, headers);
+		return restTemplate.postForObject(URI + contractId, httpEntity, String.class);
 	}
 }
