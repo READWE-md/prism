05fe0b5b115b4c24f6dbf6b23789e74d330464bd
김영표 <kyp0416@gmail.com>
2024-08-01 16:45:37 +0900

    fix: 분석 과정을 2번 호출하는 버그 수정

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -7,16 +7,17 @@ from modules.analyze_contract import analyze_contract
 load_dotenv()
 app = FastAPI()
 
+
 class Contract(BaseModel):
-    images: list[str] 
+    images: list[str]
 
 
 @app.post("/contract/{contract_id}")
 def create_contract(contract_id: int, contract_raw: Contract, background_tasks: BackgroundTasks):
-    
-    analyze_contract(contract_raw, contract_id)
+
+    # analyze_contract(contract_raw, contract_id)
     # 계약서 분석(백그라운드 처리)
-    # background_tasks.add_task(analyze_contract, contract_raw, contract_id)
-    
+    background_tasks.add_task(analyze_contract, contract_raw, contract_id)
+
     # MongoDB의 ID값 반환
     return {"_id": contract_id, "message": "Pending"}
