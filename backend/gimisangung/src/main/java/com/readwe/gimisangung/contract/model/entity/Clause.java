3903ebf8be20b393aa006c69989091412104b2e5
박찬규 <cham9906@naver.com>
2024-08-01 12:33:18 +0900

    feat: 도메인 모델 및 레파지토리 구현

--- backend/gimisangung/src/main/java/com/readwe/gimisangung/contract/model/entity/Clause.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/contract/model/entity/Clause.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
-public class Poison {
+public class Clause {
 	private String content;
 	private String result;
 
