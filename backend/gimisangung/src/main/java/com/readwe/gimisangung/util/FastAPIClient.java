5d4ca9f53f00f28de4f884a76f8a174fc4e1315c
박찬규 <cham9906@naver.com>
2024-08-01 16:59:04 +0900

    fix: 메서드 private에서 public으로 변경, Setter메서드 Annotation으로 변경

--- backend/gimisangung/src/main/java/com/readwe/gimisangung/util/FastAPIClient.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/util/FastAPIClient.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.Setter;
+
+@Setter
 public class FastAPIClient {
 
 	private static final String URI = "http://localhost:3000/contract/";
@@ -21,15 +24,7 @@ public class FastAPIClient {
 		headers.setContentType(MediaType.APPLICATION_JSON);
 	}
 
-	private void setContractId(Long contractId) {
-		this.contractId = contractId;
-	}
-
-	private void setImages(List<String> images) {
-		this.images = images;
-	}
-
-	private String sendRequest() {
+	public String sendRequest() {
 		HttpEntity<List<String>> httpEntity = new HttpEntity<>(images, headers);
 		return restTemplate.postForObject(URI + contractId, httpEntity, String.class);
 	}
