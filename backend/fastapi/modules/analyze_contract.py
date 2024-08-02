5ba441a82d7f6c05b805da7d07ecf3484496318c
허경환 <gjrudghks1@naver.com>
2024-08-02 12:52:45 +0900

    Update backend/fastapi/modules/analyze_contract.py

--- backend/fastapi/modules/analyze_contract.py
+++ backend/fastapi/modules/analyze_contract.py
@@ -217,8 +217,51 @@ class CompletionExecutor:
                 final_answer = r.json()
             return final_answer
 
+## 임베딩 API
+class EmbeddingExecutor:
+    def __init__(self, host, api_key, api_key_primary_val, request_id):
+        self._host = host
+        self._api_key = api_key
+        self._api_key_primary_val = api_key_primary_val
+        self._request_id = request_id
+ 
+    def _send_request(self, completion_request):
+        headers = {
+            "Content-Type": "application/json; charset=utf-8",
+            "X-NCP-CLOVASTUDIO-API-KEY": self._api_key,
+            "X-NCP-APIGW-API-KEY": self._api_key_primary_val,
+            "X-NCP-CLOVASTUDIO-REQUEST-ID": self._request_id
+        }
+ 
+        conn = http.client.HTTPSConnection(self._host)
+        conn.request(
+            "POST",
+            "/serviceapp/v1/api-tools/embedding/clir-emb-dolphin/04a99dcfc692405a886acf158e78c7c1", # If using Service App, change 'testapp' to 'serviceapp', and corresponding app id.
+            json.dumps(completion_request),
+            headers
+        )
+        response = conn.getresponse()
+        result = json.loads(response.read().decode(encoding="utf-8"))
+        conn.close()
+        return result
+ 
+    def execute(self, completion_request):
+        res = self._send_request(completion_request)
+        if res["status"]["code"] == "20000":
+            return res["result"]["embedding"]
+        else:
+            error_code = res["status"]["code"]
+            error_message = res.get("status", {}).get("message", "Unknown error")
+            raise ValueError(f"오류 발생: {error_code}: {error_message}")
+
 ## 사용자 쿼리를 임베딩
 def query_embed(text: str):
+    embedding_executor = EmbeddingExecutor(
+        host="clovastudio.apigw.ntruss.com",
+        api_key='-',
+        api_key_primary_val='-',
+        request_id='-'
+    )
     request_data = {"text": text}
     response_data = embedding_executor.execute(request_data)
     return response_data
