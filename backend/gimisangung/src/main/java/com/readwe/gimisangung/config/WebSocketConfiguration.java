76f38b930a55e9290e58dd35b89d6675ab4a00e5
박찬규 <cham9906@naver.com>
2024-08-08 14:00:39 +0900

    refact: 웹소켓 endpoint 변경

--- backend/gimisangung/src/main/java/com/readwe/gimisangung/config/WebSocketConfiguration.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/config/WebSocketConfiguration.java
@@ -18,7 +18,7 @@ public class WebSocketConfiguration  implements WebSocketMessageBrokerConfigurer
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/signaling")
+		registry.addEndpoint("/ws")
 			.setAllowedOriginPatterns("*");
 	}
 }
