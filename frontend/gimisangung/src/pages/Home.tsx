36527aa8c9b92682c05bb92e227c0227138a0a7f
권대형 <doto3852@naver.com>
2024-08-09 10:37:10 +0900

    홈 화면 계약서 분석 상태 수정

--- frontend/gimisangung/src/pages/Home.tsx
+++ frontend/gimisangung/src/pages/Home.tsx
@@ -306,6 +306,47 @@ const Home = () => {
     });
   };
 
+  const clickContract = (contract: Contract) => {
+    console.log(contract.status);
+    if (drawerOpen === true) {
+      selectContract(contract);
+    } else if (contract.status === "DONE") {
+      goResult(contract.id, contract.name);
+    } else {
+      alert("분석이 완료되지 않은 계약서입니다.");
+    }
+  };
+  const contractStatus = (contract: Contract) => {
+    if (contract.status === "ANALYZE_INIT") {
+      return "분석 시작";
+    } else if (
+      contract.status === "ANALYZE_OCR_START" ||
+      contract.status === "ANALYZE_OCR_DONE"
+    ) {
+      return "조항 인식 중";
+    } else if (
+      contract.status === "ANALYZE_TOKENIZE_START" ||
+      contract.status === "ANALYZE_TOKENIZE_DONE"
+    ) {
+      return "조항 분리 중";
+    } else if (
+      contract.status === "ANALYZE_CORRECTION_START" ||
+      contract.status === "ANALYZE_CORRECTION_END"
+    ) {
+      return "오타 인식 중";
+    } else if (
+      contract.status === "ANALYZE_CHECK_START" ||
+      contract.status === "ANALYZE_CHECK_END"
+    ) {
+      return "계약서 분석 중";
+    } else if (
+      contract.status === "TAG_GEN_START" ||
+      contract.status === "TAG_GEN_END"
+    ) {
+      return "태그 분류 중";
+    }
+  };
+
   return (
     <div>
       <StyledScreen>
@@ -370,9 +411,7 @@ const Home = () => {
               <ListItem
                 key={contract.id}
                 onClick={() => {
-                  drawerOpen === true
-                    ? selectContract(contract)
-                    : goResult(contract.id, contract.name);
+                  clickContract(contract);
                 }}
                 onTouchStart={() => handleTouchContractStart(contract)}
                 onTouchEnd={() => handleTouchEnd()}
@@ -411,14 +450,8 @@ const Home = () => {
                         ))}
                       </TagWrapper>
                     </div>
-                  ) : contract.status === "ANALYZE" ? (
-                    <StyledSpan>분석중</StyledSpan>
-                  ) : contract.status === "UPLOAD" ? (
-                    <StyledSpan>업로드완료</StyledSpan>
                   ) : (
-                    <StyledSpan style={{ color: "red" }}>
-                      분석에 실패하였습니다.
-                    </StyledSpan>
+                    <StyledSpan>{contractStatus(contract)}</StyledSpan>
                   )}
                 </ListContentWrapper>
               </ListItem>
