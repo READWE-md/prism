2c33741ea29d3bef62ffb36ee51e88f56b8f2fca
김영표 <kyp0416@gmail.com>
2024-08-12 10:40:12 +0900

    feat: 태그 추가 insert->select 후 update로 변경

--- backend/fastapi/modules/store_contract_tags.py
+++ backend/fastapi/modules/store_contract_tags.py
@@ -13,12 +13,18 @@ MARIADB_DB = os.getenv('MARIADB_DB')
 def store_contract_tags(contract_id: int, tag_list: list[str]):
     conn = pymysql.connect(host=MARIADB_HOST, user=MARIADB_USER, port=3306,
                            password=MARIADB_PW, db=MARIADB_DB, charset='utf8')
-
     cursor = conn.cursor()
 
-    for tag in tag_list:
-        sql = "INSERT INTO tags (name, contract_id) " + \
-            "VALUES('" + tag + "', '" + str(contract_id) + "')"
+    select_tag_id_sql = "SELECT tag_id FROM tags WHERE contract_id=" + \
+        str(contract_id)
+    cursor.execute(select_tag_id_sql)
+    result = cursor.fetchall()
+
+    for idx, tag in enumerate(tag_list):
+        tag_id = result[idx][0]
+        if tag == "":
+            tag = "."
+        sql = "UPDATE tags SET name='" + tag + "' WHERE tag_id=" + str(tag_id)
         print(sql)
         cursor.execute(sql)
     conn.commit()
