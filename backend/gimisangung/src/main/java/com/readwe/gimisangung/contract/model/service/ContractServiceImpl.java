fc0b267b31632c3f88677a2a8481966ec6af60fe
김상훈 <tpspfkdl@naver.com>
2024-08-13 14:00:45 +0900

    fix: tag의 viewedAt을 변경해야 하므로 조회 서비스들에서 @Transactional(readOnly=true) 제거

--- backend/gimisangung/src/main/java/com/readwe/gimisangung/contract/model/service/ContractServiceImpl.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/contract/model/service/ContractServiceImpl.java
@@ -1,7 +1,6 @@
 package com.readwe.gimisangung.contract.model.service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
