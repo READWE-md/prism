26b576a6a9d6aae0c2081f722b4dc8ff739d0606
김상훈 <tpspfkdl@naver.com>
2024-08-13 12:39:32 +0900

    refactor: 사용하지 않는 import 삭제

--- backend/gimisangung/src/main/java/com/readwe/gimisangung/contract/model/service/TagService.java
+++ backend/gimisangung/src/main/java/com/readwe/gimisangung/contract/model/service/TagService.java
@@ -2,9 +2,7 @@ package com.readwe.gimisangung.contract.model.service;
 
 import java.util.List;
 
-import com.readwe.gimisangung.contract.model.dto.FindTagResponseDto;
 import com.readwe.gimisangung.contract.model.entity.Contract;
-import com.readwe.gimisangung.contract.model.entity.Tag;
 import com.readwe.gimisangung.user.model.User;
 
 public interface TagService {
